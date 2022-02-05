0dc2b7db65dd3d94a90221f47264bc8be3932c73
Icarus747 <helperpsp@gmail.com>
2022-02-05 12:19:20 -0600

    Entrypoint for 404 missing /

--- app/404.sh
+++ app/404.sh
@@ -1,4 +1,4 @@
-tail /config/log/nginx/access.log | awk '/zip.+404/{print $7}' | awk -F '/' '{print $3}' >> config/limitdir/limit_list.tmp
+tail /config/log/nginx/access.log | awk '/zip.+404/{print $7}' | awk -F '/' '{print $3}' >> /config/limitdir/limit_list.tmp
 
 sort -u /config/limitdir/limit_list.tmp > /config/limitdir/limit_list.db
 
